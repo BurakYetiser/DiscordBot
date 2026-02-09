b8de5a8b8154235973716fa55165aaa3345704aa
BurakYetiser <burakyetiser04@gmail.com>
2026-02-09 07:06:36 +0300

    .

--- DiscordBot/obj/Debug/net8.0/DiscordBot.AssemblyInfo.cs
+++ DiscordBot/obj/Debug/net8.0/DiscordBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+023ec491105366de10fd82ba22a083833cdec54a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21a4a917ca57535d5af605306b3c01e0002d21cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiscordBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
